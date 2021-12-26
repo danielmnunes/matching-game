c7ba6d752f1371496754350bf3693339129b0819
Daniel Nunes <danielmnunes08@gmail.com>
2021-12-26 12:02:25 -0300

    deploy

--- assets/index.b493d7f1.js
+++ assets/index.b493d7f1.js
@@ -1,5 +1,5 @@
 const i=function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&n(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}};i();const l=()=>`
         <article class="card-game">
-          <img src="images/alura-pixel.png" alt="Logo da Alura">
+          <img src="/assets/img/alura-pixel.png" alt="Logo da Alura">
         </article>
     `,c=document.querySelector("#root"),a=l();c.insertAdjacentHTML("beforeend",a);
