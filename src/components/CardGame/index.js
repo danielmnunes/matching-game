bcee898e5b64a60fef4298cdc04fbdbaa2ab0c3c
Daniel Nunes <danielmnunes08@gmail.com>
2021-12-26 12:11:56 -0300

    create cardGame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+const CardGame = () => {
+    return /*html*/`
+        <article class="card-game">
+          <img src="/matching-game/assets/img/alura-pixel.png" alt="Logo da Alura">
+        </article>
+    `
+}
+
+export default CardGame;
