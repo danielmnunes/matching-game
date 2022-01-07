e62ef33c2c900710cdb67af7614de307684321ed
Daniel Nunes <danielmnunes08@gmail.com>
2022-01-07 12:40:15 -0300

    add VsPlayer

---
+++ src/components/VsPlayer/index.js
@@ -0,0 +1,5 @@
+import './style.css'
+
+export default (content = 'vs') => {
+  return /* html */`<span class='vs-player'>${content}</span>`
+}
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,10 +1,12 @@
 import './style.css'
 import PlayerGame from '../../components/PlayerGame'
+import VsPlayer from '../../components/VsPlayer'
 
 export default () => {
   return /* html */`
         <header class='score-board'>
             ${PlayerGame('Player1')}
+            ${VsPlayer()}
             ${PlayerGame('Player2')}
         </header>
     `
