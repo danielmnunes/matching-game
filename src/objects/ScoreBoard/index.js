c14568d4d546d87da271e401395c4e5571c9b0e6
Daniel Nunes <danielmnunes08@gmail.com>
2022-01-07 16:01:26 -0300

    add ScoreBoard component

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,12 +1,15 @@
 import './style.css'
 import PlayerGame from '../../components/PlayerGame'
 import VsPlayer from '../../components/VsPlayer'
+import PlayerScore from '../../components/PlayerScore'
 
 export default () => {
   return /* html */`
         <header class='score-board'>
             ${PlayerGame('Player1')}
+            ${PlayerScore(1)}
             ${VsPlayer()}
+            ${PlayerScore(2)}
             ${PlayerGame('Player2')}
         </header>
     `
